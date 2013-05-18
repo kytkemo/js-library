d35786228076afae45f1ffeca103cb25da3c21f9
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-05-18 19:20:37 +0300

    1.0

---
+++ Gruntfile.js
@@ -0,0 +1,102 @@
+module.exports = function(grunt) {
+
+    /* Configuration */
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+
+            src: {
+
+                files: 'src/**/*.js',
+                tasks: ['jshint:src', 'build'],
+                options: {
+
+                    nospawn: true
+
+                }
+            }
+        },
+
+        concat: {
+
+            dist: {
+
+                src: 'src/**/*.js',
+                dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.js',
+                options: {
+
+                    separator: ';'
+
+                }
+            }
+        },
+
+        uglify: {
+
+            dist: {
+
+                files: {
+
+                    'dist/<%= pkg.name %>-<%= pkg.version %>-min.js': 'dist/<%= pkg.name %>-<%= pkg.version %>.js'
+
+                },
+
+                options: {
+
+                    report: 'min'
+
+                }
+            }
+        },
+
+        jshint: {
+
+            src: {
+
+                src: ['Gruntfile.js', 'src/**/*.js'],
+                options: {
+
+                    jshintrc: 'jshint.json'
+
+                }
+            },
+
+            tests: {
+
+                src: 'spec/**/*.js',
+                options: {
+
+                    jshintrc: 'spec/jshint.json'
+
+                }
+            }
+        },
+
+        jasmine: {
+
+            src: 'src/**/*.js',
+            options: {
+
+                specs: 'spec/**/*-spec.js'
+
+            }
+        }
+    });
+
+    /* Load tasks */
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+    /* Register tasks */
+
+    grunt.registerTask('test', ['jshint', 'jasmine']);
+    grunt.registerTask('build', ['concat', 'uglify']);
+    grunt.registerTask('default', ['test', 'build']);
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# js-library
+
+Structure for a JavaScript library (JavaScript + Node.js + Grunt).
+
+## Install dependencies
+
+Install Grunt's command line interface globally if necessary with `npm install -g grunt-cli`. Download development 
+dependencies by running `npm install -d`.
+
+## Run tests
+
+Run tests with `grunt test`.
+
+## Build
+
+Build from source with `grunt build`.
+
+## Watch
+
+To build from source automatically when files are changed run `grunt watch`.
